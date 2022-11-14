693e165b91d42acb1d5b29f8bd2dc5801c151c40
nayim-hossen <nayimh2017@gmail.com>
2022-11-15 04:15:59 +0600

    cleare all unused file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3264DA",
+
+          secondary: "#355FD9",
+
+          accent: "#0E254E",
+
+          neutral: "#BECDCE",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  //...
+};
