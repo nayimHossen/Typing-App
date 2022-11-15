1f3025cd67015e27a471896faa970b02a5f3bf27
nayim-hossen <nayimh2017@gmail.com>
2022-11-15 23:40:01 +0600

    add some feature

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import { formatPercentage } from "../utils/helper";
+
 const Results = ({
   errors,
   accuracyPercentage,
@@ -14,14 +17,42 @@ const Results = ({
   const duration = { duration: 0.3 };
 
   return (
-    <ul
+    <motion.ul
+      initial={initial}
+      animate={animate}
       className={`flex flex-col items-center text-primary space-y-3 ${className}`}
     >
-      <li className="text-xl font-smaibold">Result</li>
-      <li>Accuracy: {accuracyPercentage}</li>
-      <li className="text-red-100">Errors: {errors}</li>
-      <li>Type: {total}</li>
-    </ul>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3 }}
+        className="text-xl font-semibold"
+      >
+        Results
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 0.5 }}
+      >
+        Accuracy: {formatPercentage(accuracyPercentage)}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 1 }}
+        className="text-red-500"
+      >
+        Errors: {errors}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 1.4 }}
+      >
+        Typed: {total}
+      </motion.li>
+    </motion.ul>
   );
 };
 
